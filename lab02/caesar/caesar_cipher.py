a7070dc081669f8d70b8eeea2e72bdd6c1f98b69
lehongphi806 <lehongphi806@example.com>
2024-04-11 21:04:56 +0700

    [add] lab02

--- lab02/caesar/caesar_cipher.py
+++ lab02/caesar/caesar_cipher.py
@@ -1,26 +1,27 @@
-from cipher.caesar import ALPHABET
+from caesar import ALPHABET
+
 class CaesarCipher:
     def __init__(self):
         self.alphabet = ALPHABET
-    def encrypt_text(self, text: str, key: int)-> str:
+        
+    def encrypt_text(self, text:str, key:int) -> str:
         alphabet_len = len(self.alphabet)
         text = text.upper()
-        encrypt_text =[]
+        encrypt_text = []
         for letter in text:
             letter_index = self.alphabet.index(letter)
-            output_index =(letter_index + key )% alphabet_len 
-            
+            output_index = int((letter_index + key) % alphabet_len)
             output_letter = self.alphabet[output_index]
             encrypt_text.append(output_letter)
         return "".join(encrypt_text)
-    def decrypt_text(self, text: str, key: int )->str:
-        alphabet_len =len(self.alphabet)
+    
+    def decrypt_text(self, text:str, key:int) -> str:
+        alphabet_len = len(self.alphabet)
         text = text.upper()
-        decrypted_text =[]
+        decrypt_text = []
         for letter in text:
             letter_index = self.alphabet.index(letter)
-            output_index =(letter_index - key )% alphabet_len 
-            
+            output_index = int((letter_index - key) % alphabet_len)
             output_letter = self.alphabet[output_index]
-            decrypted_text.append(output_letter)
-        return "".join(decrypted_text)
+            decrypt_text.append(output_letter)
+        return "".join(decrypt_text)
